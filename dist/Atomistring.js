d2f147a91f0f1398bb4a07e87527b0bb6a0bcedc
Airscripts <dev.airscript@gmail.com>
2020-11-19 08:26:33 -0800

    [2020/11/19] Added package.json into dist/.

--- dist/Atomistring.js
+++
@@ -1,7 +0,0 @@
-// Importing: Data.
-import icons from "./Icons";
-// Declaring Atomicon Icon.
-export function Atomistring(_a) {
-    var type = _a.type, icon = _a.icon, theme = _a.theme;
-    return icons[type][theme][icon];
-}
